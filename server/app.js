72c538b9da5ef73569b10bc3ff1299ec100246cd
arpit9839 <arpit4499@gmail.com>
2023-06-23 01:27:19 +0530

    middleware 404 and 400 error done..

--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const env = require('dotenv');
 const app = express();
-const config = require("./config")
+const config = require("./config");
+const {PageNotFound,intrnlservrEror, BadReq} = require('./middleware/errors');
 
 if (process.env.NODE_ENV !== 'production') {
     env.config()
 }
 
-const PORT = config.port || 5000;
+app.get('/',(req,res)=>{
+    res.send(req.httpVersion)
+});
+
+app.all(BadReq);
+app.all("*",PageNotFound)
+app.use((err,req,res,next)=>{
+    err.statusCode =err.statusCode ||500;
+    err.status = err.status || "error found" 
+res.status(err.statusCode).json({
+    staus:err.statusCode,
+    message: err.message,
+})
+})
+
+const PORT = process.env.PORT|| 5000;
 app.listen(PORT , console.log(`server started on ${PORT}`));
