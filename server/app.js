961fbb415e6b34fb1f54efa6359e0cd8957d4b52
Neeraj Verma <neerajverma101@gmail.com>
2023-07-13 00:59:47 +0530

    login integration (#32)

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const env = require('dotenv');
 
-const {PageNotFound, BadReq, ReqError} = require('./middleware/errors');
+const { PageNotFound, BadReq, ReqError } = require('./middleware/errors');
 const dbConnect = require('./config/dbConnect');
 const errorHandler = require('./middleware/errorHandler');
 const vendorRouter = require('./Routes/vendorRouter');
 const userRouter = require('./Routes/userRouter')
 const app = express();
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors())
+
 env.config();
 //dataBase connection
 dbConnect();
@@ -23,13 +27,13 @@ app.get('/', (req, res) => {
 //error and errorHandler section
 
 
-app.use('/api/vendor',vendorRouter);
-app.use('/api/User',userRouter);
+app.use('/api/vendor', vendorRouter);
+app.use('/api/User', userRouter);
 
 app.all(ReqError);
 app.all(BadReq);
 
-app.all("*",PageNotFound)
+app.all("*", PageNotFound)
 app.use(errorHandler)
 app.all("*", PageNotFound)
 app.use((err, req, res, next) => {
