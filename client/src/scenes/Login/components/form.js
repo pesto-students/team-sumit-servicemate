a28c6b22e11b8b9b54b08c211bfac66124ced205
arpit9839 <arpit4499@gmail.com>
2023-07-30 15:00:53 +0530

    login Page ui enhanceent done

--- client/src/scenes/Login/components/form.js
+++ client/src/scenes/Login/components/form.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react'
-import "./styles/form.scss"
-import { Button, TextField } from '@mui/material';
-// import { data } from '../../../config/db';
-import ErrorMessage from './error';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ErrorMessage from './error'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 // import restClient from '../../../config/axios';
 import { useDispatch } from 'react-redux';
 import { setLoggedInUser } from '../actions';
-import restClient from '../../../config/axios';
+//import restClient from '../../../config/axios';
+import axios from 'axios';
 
-const LoginForm = () => {
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" >
+        ServiceMate
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+
+const defaultTheme = createTheme();
+
+
+export default function SignIn() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const [formData, setFormData] = useState({})
     const [showError, setShowError] = useState(false)
     const [loading, setLoading] = useState(false)
     console.log("🚀 ~ file: form.js:15 ~ LoginForm ~ loading:", loading)
-
+    
     const handleFormChange = (e) => {
         const { name, value } = e ? e.target : {}
         if (name) {
@@ -29,13 +59,13 @@ const LoginForm = () => {
         e.preventDefault()
         setLoading(true);
         try {
-
+    
             const config = {
                 headers: {
                     "content-type": "application/json",
                 },
             };
-            const { data } = await restClient.post("/api/User/login", { ...formData }, config);
+            const { data } = await axios.post("https://5000-pestostuden-teamsumitse-o9vtf8gtizt.ws-us102.gitpod.io/api/user/login", { ...formData }, config);
             // localStorage.setItem("userInfo", JSON.stringify(data));
             dispatch(setLoggedInUser(data))
             setShowError(false)
@@ -46,18 +76,76 @@ const LoginForm = () => {
         }
     }
 
-    // const isUserExist = () => {
-    //     return data.users.find(user => user.emailId === formData.emailId && user.password === formData.password)
-    // }
-
-    return (
-        <form className="registration-form">
-            <TextField label="Email" variant="outlined" name='email' onChange={handleFormChange} />
-            <TextField label="Password" variant="outlined" type="password" name="password" onChange={handleFormChange} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name='email'
+              autoComplete="email"
+              autoFocus
+              onChange={handleFormChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleFormChange}
+            />
             <ErrorMessage open={showError} handleClose={() => { setShowError(false) }} message='you have entered invalid credentials'></ErrorMessage>
-            <Button type='submit' variant="contained" color="primary" onClick={handleLogin}>Login</Button>
-        </form>
-    );
-};
-
-export default LoginForm;
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
