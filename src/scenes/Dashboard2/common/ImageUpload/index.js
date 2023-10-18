0a54d3b87876cab3fad3fdf530b28d27c5f7e7f2
Neeraj Verma <neerajverma101@gmail.com>
2023-10-19 02:26:54 +0530

    Landing-page-integration (#82)
    * land page api integration
    
    * added alert noti and home api integration changes
    
    * admin dashboard category changes
    
    * hear and dashboard2 changes
    
    * add snackbar and logout
    
    * added routes constants
    
    * added route constant and update routes
    
    * landing page responsive changes
    
    * Merge branch 'develop' into landing-page-integration
    
    * add services changes
    
    * add services changes
    
    * code refcatoring for landing page
    
    * landing page design changes and add service freelancer api integration
    
    * landing page design changes and add service freelancer api integration
    
    * upload vendor image
    
    * added 404 page
    
    * vendor dashboard my profile changes
    
    * add services image upload

--- src/scenes/Dashboard2/common/ImageUpload/index.js
+++ src/scenes/Dashboard2/common/ImageUpload/index.js
@@ -5,6 +5,7 @@ import { SfThumbnail } from '@storefront-ui/react';
 import PersonIcon from '@mui/icons-material/Person';
 import { PropTypes } from 'prop-types';
 import { Box } from '@mui/system';
+import { useEffect } from 'react';
 const VisuallyHiddenInput = styled('input')`
   clip: rect(0 0 0 0);
   clip-path: inset(50%);
@@ -18,45 +19,54 @@ const VisuallyHiddenInput = styled('input')`
 `;
 
 const ImageUpload = (props) => {
-  const { label = "Upload a photo", handleImageUpload, image, id = 'image', name = 'image', alt = 'image' } = props;
-  const [imagePreview, setImagePreview] = useState(null)
+  const { label = 'Upload a photo', handleImageUpload, image, id = 'image', name = 'image', alt = 'image', multiple = false, images = [] } = props;
+  const [imagePreview, setImagePreview] = useState(null);
 
   const handleFileUpload = (e) => {
     e.preventDefault();
+    const selectedFiles = multiple ? e.target.files : e.target.files[0];
+    const previewFiles = multiple ? getPreviewFromFiles(selectedFiles) : URL.createObjectURL(selectedFiles);
+    setImagePreview(previewFiles);
+    handleImageUpload(selectedFiles, previewFiles);
+  };
 
-    const reader = new FileReader();
-    const file = e.target.files[0];
+  const getPreviewFromFiles = (files) => {
+    return Object.values(files).map(url => URL.createObjectURL(url));
+  };
 
-    reader.onloadend = () => {
-      setImagePreview(reader.result);
-      handleImageUpload({ [id]: file })
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  }
-
+  useEffect(() => {
+    if (images?.length)
+      setImagePreview(typeof images?.[0] === 'object' ? getPreviewFromFiles(images) : images);
+  }, [images]);
 
 
   return (
     <Grid cont>
-      <Box sx={{ height: 150, width: 150 }}>
-        {image ?
-          <SfThumbnail size="xl" className='w-full h-full'>
-            <img style={{ height: '100%', objectFit: 'cover' }} src={imagePreview || image} alt={alt}></img>
-          </SfThumbnail>
-          :
-          <PersonIcon sx={{ height: 150, width: 150 }}></PersonIcon>
-        }
-      </Box>
+      {multiple ?
+        <Grid container spacing={imagePreview?.length && 4}>
+          {imagePreview?.map((image, imageIndex) => (
+            <Grid item key={'image-' + imageIndex}>
+              <img style={{ height: '100%', objectFit: 'cover' }} src={image} alt={alt}></img>
+            </Grid>
+          ))}
+        </Grid>
+        :
+        <Box sx={{ height: 150, width: 150 }}>
+          {image ?
+            <SfThumbnail size="xl" className='w-full h-full'>
+              <img style={{ height: '100%', objectFit: 'cover' }} src={imagePreview || image} alt={alt}></img>
+            </SfThumbnail>
+            :
+            <PersonIcon sx={{ height: 150, width: 150 }}></PersonIcon>
+          }
+        </Box>}
       <Button
         component="label"
         variant="text"
         startIcon={<CloudUploadIcon />}
       >
         {label}
-        <VisuallyHiddenInput onChange={handleFileUpload} type="file" accept="image/*" id={id} name={name}
+        <VisuallyHiddenInput onChange={handleFileUpload} type="file" multiple accept="image/*" id={id} name={name}
         />
       </Button>
     </Grid>
@@ -72,4 +82,6 @@ ImageUpload.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   alt: PropTypes.string,
-}
+  multiple: PropTypes.bool,
+  images: PropTypes.array
+};
